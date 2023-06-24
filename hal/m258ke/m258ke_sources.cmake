8aba5b6d103e60617c8f921b77b7ff05aa7ede9a
Doug Brown <doug@schmorgal.com>
2023-06-24 12:19:22 -0700

    Add cmake source/options includes for M258KE port

---
+++ hal/m258ke/m258ke_sources.cmake
@@ -0,0 +1,54 @@
+set(HWSOURCES
+	SIMMProgrammer/hal/m258ke/nuvoton/core_cm23.h
+	SIMMProgrammer/hal/m258ke/nuvoton/cmsis_armcc.h
+	SIMMProgrammer/hal/m258ke/nuvoton/cmsis_armclang.h
+	SIMMProgrammer/hal/m258ke/nuvoton/cmsis_compiler.h
+	SIMMProgrammer/hal/m258ke/nuvoton/cmsis_gcc.h
+	SIMMProgrammer/hal/m258ke/nuvoton/cmsis_version.h
+
+	SIMMProgrammer/hal/m258ke/nuvoton/NuMicro.h
+	SIMMProgrammer/hal/m258ke/nuvoton/M251.h
+	SIMMProgrammer/hal/m258ke/nuvoton/system_M251.c
+	SIMMProgrammer/hal/m258ke/nuvoton/system_M251.h
+	SIMMProgrammer/hal/m258ke/nuvoton/startup_M251.S
+
+	SIMMProgrammer/hal/m258ke/nuvoton/acmp_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/bpwm_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/clk_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/crc_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/crypto_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/dac_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/eadc_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/ebi_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/fmc_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/gpio_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/i2c_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/lcd_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/opa_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/pdma_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/psio_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/pwm_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/qspi_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/rtc_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/sc_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/spi_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/sys_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/timer_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/tk_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/uart_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/ui2c_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/usbd_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/uspi_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/uuart_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/wdt_reg.h
+	SIMMProgrammer/hal/m258ke/nuvoton/wwdt_reg.h
+
+	SIMMProgrammer/hal/m258ke/nuvoton/usbd.c
+	SIMMProgrammer/hal/m258ke/nuvoton/usbd.h
+	SIMMProgrammer/hal/m258ke/usbcdc.c
+	SIMMProgrammer/hal/m258ke/usbcdc_hw.h
+	SIMMProgrammer/hal/m258ke/descriptors.c
+
+	hal/m258ke/hardware.c
+	hal/m258ke/hardware.h
+)
